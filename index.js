ed8569a9646e0533c981d320e26647b86b8690a1
Adreena <hurlocene@gmail.com>
2021-04-28 16:42:03 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(stringName, eventName) {
+    let thankYouCard = [];
+    for(let i = 0; i < stringName.length; i++){
+        thankYouCard.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCard;
+}
+
+writeCards(["Evie", "Nicole", "Mary"], "birthday");
+
+function countDown(number){
+    while(number>=0) {
+        console.log(number);
+        number--;
+    }
+}
